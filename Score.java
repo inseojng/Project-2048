c3db0498d567e85b6e7660b95287ff217097d86d
Inseo <inseojung.01@gmail.com>
2022-07-12 21:38:53 +0900

    Add files via upload

---
+++ Score.java
@@ -0,0 +1,72 @@
+import java.io.*;
+
+
+public class Score {
+
+    private int score = 0;
+    private int highestScore;
+
+    public Score() {
+        this.score = 0;
+        this.highestScore = 0;
+    }
+
+    public Score(int _score, int _highestScore) {
+        this.score = _score;
+        this.highestScore = _highestScore;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public int getHighestScore() {
+        return this.highestScore;
+    }
+
+    public void addScore(int val) {
+        this.score += val;
+    }
+
+    public int setScore(int _score) {
+        this.score = _score;
+        return this.score;
+    }
+
+    public int setHighestScore(int _highestScore) {
+        this.highestScore = _highestScore;
+        return this.highestScore;
+    }
+
+    public void updateHighestScore() {
+        if (score > highestScore) {
+            highestScore = this.score;
+        }
+    }
+
+    public static void saveScore(Score score, File file) {
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter(file));
+            bw.write("" + score.highestScore);
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            //todo exception handling.
+        }
+    }
+
+    public static Score loadScore(File file){
+        BufferedReader br = null;
+        Score s = new Score();
+        try {
+            br = new BufferedReader(new FileReader(file));
+            s.highestScore = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+            //todo fail to read error msg.
+        }
+        return s;
+    }
+
+}
